345a35714305cb03e47b7c823e61d28069162db5
Sofia Mashkovtseva <mashkovtseva.ssff@gmail.com>
2022-05-25 01:21:07 +0300

    add my last home work

---
+++ index.js
@@ -0,0 +1,6 @@
+const question = prompt('What is your name?');
+alert ('Hello, ' + question + '! How are you?');
+
+// alternative variant:
+// const question = prompt('What is your name?');
+// alert(`Hello, ${question}! How are you?`)
